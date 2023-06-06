c4fe59cdb7d94a752432e9b4c4f0d9f0051b9adc
Zubair Zulifqar <91710951+Zubair-Zulifqar@users.noreply.github.com>
2023-06-07 00:42:08 +0500

    Borower Object contain Data of person borrow the book

---
+++ BorowerObject/BorowerObject/BorowerObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorowerObject
+{
+    public class BorowerObjectClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public string rollNo { set; get; }
+
+        public string libraryCardNumber { set; get; }
+
+        public int bookId { get; set; }
+    }
+}
